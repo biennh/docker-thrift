b816ec9174b51f9b08b459bbcd72aaa5590abb8d
hnguyen <hnguyen@roku.com>
2021-10-11 20:42:35 -0700

    Update docker file for thrift 0.10 and 0.13

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker build -t docker-thrift:0.8.0 ./0.8/
+docker build -t docker-thrift:0.10.0 ./0.10/
+docker build -t docker-thrift:0.13.0 ./0.13/
+docker save docker-thrift:0.8.0 | gzip > docker-thrift_0.8.0.tar.gz
+docker save docker-thrift:0.10.0 | gzip > docker-thrift_0.10.0.tar.gz
+docker save docker-thrift:0.13.0 | gzip > docker-thrift_0.13.0.tar.gz
